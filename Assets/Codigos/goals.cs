a46d6a4eb8b45438696ee09105d2f64fd125c4a8
PabloSaura <pablosaauraa@gmail.com>
2023-04-03 17:36:26 +0200

    COMMIT DE INSTALACIÓN

--- Assets/Codigos/goals.cs
+++ Assets/Codigos/goals.cs
@@ -21,7 +21,7 @@ public class goals : MonoBehaviour
 }
     void OnTriggerEnter2D(Collider2D otro){
         principalScript.score += 1;
-        gestorSonido.GetComponent<sonidoGoals>()
+       // gestorSonido.GetComponent<sonidoGoals>()
      }
 
 }
